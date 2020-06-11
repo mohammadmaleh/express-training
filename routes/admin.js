c2b1c27f569d64605a667225ef39c3d5fc9e1a6d
Mohammad Maleh <mohammad.maleh@gmail.com>
2020-06-11 16:49:19 +0300

    -

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
+const product = [];
 
 const router = express.Router();
 const products = [];
@@ -13,8 +14,10 @@ router.get("/add-product", (req, res, next) => {
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+
   console.log(req.body);
   res.redirect("/");
 });
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+const adminData = require("./admin");
+router.get("/", (req, res, next) => {
+  const { products } = adminData;
+  res.render("shop", { products, docTitle: "Shop" });
 });
 
 module.exports = router;
